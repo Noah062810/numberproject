7ab12895091043f372d58005a3588d4637fceaf7
Noah062810 <122543656+Noah062810@users.noreply.github.com>
2023-01-26 09:39:05 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    numbercount += 1
+    basic.showNumber(numbercount,50)
+})
+input.onButtonPressed(Button.AB, function () {
+    numbercount = 0
+    basic.showNumber(numbercount,50)
+})
+input.onButtonPressed(Button.B, function () {
+    numbercount += -1
+    basic.showNumber(numbercount,50)
+})
+let numbercount = 0
+numbercount = 0
+basic.showNumber(numbercount,50)
